284c029e43d3b2a0f25467af3827bab5c0a0db3b
43696467 <43696467@qq.com>
2016-10-11 18:00:57 +0800

    change

---
+++ backgroundRunTest/Build/Intermediates/backgroundRunTest.build/Debug-iphoneos/backgroundRunTest.build/Objects-normal/arm64/NSString+HMACSHA1.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/sha1/NSString+HMACSHA1.m \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/sha1/NSString+HMACSHA1.h \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/sha1/GTMBase64.h \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/sha1/GTMDefines.h
---
+++ backgroundRunTest/Build/Intermediates/backgroundRunTest.build/Debug-iphoneos/backgroundRunTest.build/Objects-normal/arm64/NSString+safe.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/base/NSString+safe.m \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/base/NSString+safe.h \
+  /Users/a43696467/Documents/guide/backgroundRunTest/backgroundRunTest/sha1/NSString+HMACSHA1.h
